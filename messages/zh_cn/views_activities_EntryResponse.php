56c77604a699375be76de0c632e86d3d57328ff8
hbma <hbma@terdon.com>
2014-10-17 15:12:28 +0800

    Add the zh_cn language for calendar

---
+++ messages/zh_cn/views_activities_EntryResponse.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  '%displayName% attends to %contentTitle%.' => '%displayName% hat für %contentTitle% zugesagt.',
+  '%displayName% maybe attends to %contentTitle%.' => '%displayName% 参加了 %contentTitle% teil.',
+  '%displayName% not attends to %contentTitle%.' => '%displayName% 没有参加 %contentTitle% teil.',
+);
