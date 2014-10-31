b3353db4be7e015c18aff0c2cf6cbacaa302c759
Lucas Bartholemy <lucas@bartholemy.com>
2014-10-31 13:31:38 +0100

    Updated translations (translate.humhub.org)

--- messages/de/views_activities_EntryCreated.php
+++ messages/de/views_activities_EntryCreated.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  '%displayName% created a new %contentTitle%.' => '%displayName% erstellte neues %contentTitle%.',
+  '%displayName% created a new %contentTitle%.' => '%displayName% erstellte den neuen Termin »%contentTitle%«.',
 );
--- messages/de/views_activities_EntryResponse.php
+++ messages/de/views_activities_EntryResponse.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  '%displayName% attends to %contentTitle%.' => '%displayName% hat für %contentTitle% zugesagt.',
-  '%displayName% maybe attends to %contentTitle%.' => '%displayName% nimmt vielleicht bei %contentTitle% teil.',
-  '%displayName% not attends to %contentTitle%.' => '%displayName% nimmt nicht bei %contentTitle% teil.',
+  '%displayName% attends to %contentTitle%.' => '%displayName% hat für »%contentTitle%« zugesagt.',
+  '%displayName% maybe attends to %contentTitle%.' => '%displayName% nimmt vielleicht bei »%contentTitle%« teil.',
+  '%displayName% not attends to %contentTitle%.' => '%displayName% nimmt nicht bei »%contentTitle%« teil.',
 );
