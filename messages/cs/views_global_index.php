be0df2e0d9aa9978d4eace33c93136c17e902e06
Lucas Bartholemy <lucas@bartholemy.com>
2014-11-30 20:12:00 +0100

    Updated translation (translate.humhub.org)

--- messages/cs/views_global_index.php
+++ messages/cs/views_global_index.php
@@ -2,12 +2,12 @@
 return array (
   '<strong>Filter</strong> events' => '<strong>Filtrovat</strong> události',
   '<strong>Select</strong> calendars' => '<strong>Vybrat</strong> kalendáře',
-  'Already responded' => '',
+  'Already responded' => 'Již odpovězeno',
   'Followed spaces' => 'Sledované prostory',
   'Followed users' => 'Sledovaní uživatelé',
   'I´m attending' => 'Účastním se',
   'My events' => 'Moje události',
   'My profile' => 'Můj profil',
   'My spaces' => 'Moje prostory',
-  'Not responded yet' => '',
+  'Not responded yet' => 'Zatím neodpovězeno',
 );
