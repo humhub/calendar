d3861755c44b943b68b2a35300db573c291d15bf
Lucas Bartholemy <lucas@bartholemy.com>
2015-04-02 00:32:21 +0200

    Added Arabic language

---
+++ messages/ar/views_entry_view.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'Attend' => '',
+  'Created by:' => '',
+  'Decline' => '',
+  'Edit this event' => '',
+  'I´m attending' => '',
+  'I´m maybe attending' => '',
+  'I´m not attending' => '',
+  'Maybe' => '',
+  'Public' => '',
+);
