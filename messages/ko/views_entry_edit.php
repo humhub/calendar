d7127cad53d20aa84d3ecf8d32d1bcbf844cac11
Lucas Bartholemy <lucas@bartholemy.com>
2015-06-02 16:52:15 +0200

    Added Indonesian & Koreanic language files

---
+++ messages/ko/views_entry_edit.php
@@ -0,0 +1,18 @@
+<?php
+return array (
+  '<strong>Create</strong> event' => '',
+  '<strong>Edit</strong> event' => '',
+  '<strong>Note:</strong> This event will be created on your profile. To create a space event open the calendar on the desired space.' => '',
+  'Close' => '',
+  'Delete' => '',
+  'Description' => '',
+  'End Date' => '',
+  'End Date/Time' => '',
+  'Everybody can participate' => '',
+  'No participants' => '',
+  'Participants' => '',
+  'Save' => '',
+  'Start Date' => '',
+  'Start Date/Time' => '',
+  'Title' => '',
+);
