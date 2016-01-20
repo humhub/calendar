1f73396ae725cd92c0d32b177397b3c223ec8c6b
HumHub Translate <info@humhub.com>
2016-01-20 14:15:31 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/views_activities_EntryCreated.php
+++ messages/de/views_activities_EntryCreated.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  '%displayName% created a new %contentTitle%.' => '%displayName% erstellte den neuen Termin »%contentTitle%«.',
+  '%displayName% created a new %contentTitle%.' => '%displayName% hat einen neuen Termin »%contentTitle%« erstellt.',
 );
--- messages/tr/views_global_index.php
+++ messages/tr/views_global_index.php
@@ -2,12 +2,12 @@
 return array (
   '<strong>Filter</strong> events' => '<strong>Etkinlik</strong> filtresi',
   '<strong>Select</strong> calendars' => '<strong>Takvim</strong> seç',
-  'Already responded' => 'Zaten cevaplandı',
-  'Followed spaces' => 'Takip edilen mekan',
-  'Followed users' => 'Takip edilen kullanıcı',
+  'Already responded' => 'Cevaplananlar',
+  'Followed spaces' => 'Mekan',
+  'Followed users' => 'Kullanıcı',
   'I´m attending' => 'Katılıyorum',
   'My events' => 'Etkinliklerim',
   'My profile' => 'Profilim',
   'My spaces' => 'Mekanım',
-  'Not responded yet' => 'Henüz cevaplanmadı',
+  'Not responded yet' => 'Cevaplanmadı',
 );
