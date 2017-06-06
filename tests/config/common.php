840220969ceaa56ddc45fad6deaf788033afe7a0
buddh4 <mail@jharrer.de>
2017-06-06 19:10:04 +0200

    Test config alignment

--- tests/config/common.php
+++ tests/config/common.php
@@ -2,8 +2,4 @@
 /**
  * This config is shared by all suites (unit/functional/acceptance) and can be overwritten by a suite config (e.g. functional.php)
  */
-return [
-    'params' => [
-        'moduleAutoloadPaths' => ['E:\codebase\humhub\modules'],
-    ]
-];
+return [];
