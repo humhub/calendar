03e708b53482e5039dc7e9008379fa77e4e46c8b
HumHub Translations <info@humhub.com>
2017-06-21 15:40:04 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/views_entry_edit.php
+++ messages/hu/views_entry_edit.php
@@ -5,7 +5,7 @@ return array (
   '<strong>Note:</strong> This event will be created on your profile. To create a space event open the calendar on the desired space.' => '',
   'Close' => 'Zatvori',
   'Delete' => 'Törlés',
-  'Description' => '',
+  'Description' => 'Leírás',
   'End Date/Time' => '',
   'Everybody can participate' => '',
   'No participants' => '',
