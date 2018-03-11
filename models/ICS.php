56751b4026106e815afd2d72659047f20422f95b
Adam Bajac <ASBAdmin@ASB-HYPERV-1.asbd.internal>
2018-03-11 15:58:40 -0600

    Re-wrote the ICS class in a more manual, but more flexible manner, and introduced support for time zones, since UTC was not working for me. The timezone will be obtained based on the timezone setting in HumHub. The ICS file generated has been tested with S Planner and HumHub, and seems to work correctly.
    Signed-off-by: Adam Bajac <ASBAdmin@ASB-HYPERV-1.asbd.internal>

--- models/ICS.php
+++ models/ICS.php
@@ -44,32 +44,23 @@
 namespace humhub\modules\calendar\models;
 
 class ICS {
-  const DT_FORMAT = 'Ymd\THis\Z';
+  const DT_FORMAT = 'Ymd\THis';
+  protected $summary;
+  protected $description;
+  protected $dtstart;
+  protected $dtend;
+  protected $location;
+  protected $url;
+  protected $timezone;
 
-  protected $properties = array();
-  private $available_properties = array(
-    'description',
-    'dtend',
-    'dtstart',
-    'location',
-    'summary',
-    'url'
-  );
-
-  public function __construct($props) {
-    $this->set($props);
-  }
-
-  public function set($key, $val = false) {
-    if (is_array($key)) {
-      foreach ($key as $k => $v) {
-        $this->set($k, $v);
-      }
-    } else {
-      if (in_array($key, $this->available_properties)) {
-        $this->properties[$key] = $this->sanitize_val($val, $key);
-      }
-    }
+  public function __construct($summary, $description, $dtstart, $dtend, $location, $url, $timezone) {
+    $this->summary = $this->escape_string($summary);
+    $this->description = $this->escape_string($description);
+    $this->dtstart = $this->format_timestamp($dtstart);
+    $this->dtend = $this->format_timestamp($dtend);
+    $this->location = $this->escape_string($location);
+    $this->url = $this->escape_string($url);
+    $this->timezone = $timezone;
   }
 
   public function to_string() {
@@ -78,49 +69,26 @@ class ICS {
   }
 
   private function build_props() {
+
     // Build ICS properties - add header
     $ics_props = array(
       'BEGIN:VCALENDAR',
       'VERSION:2.0',
       'PRODID:-//hacksw/handcal//NONSGML v1.0//EN',
       'CALSCALE:GREGORIAN',
-      'BEGIN:VEVENT'
+      'BEGIN:VEVENT',
+      'LOCATION:' . $this->location,
+      'DESCRIPTION:' . $this->description,
+      'DTSTART;TZID=' . $this->timezone . ':' . $this->dtstart,
+      'DTEND;TZID=' . $this->timezone . ':' . $this->dtend,
+      'SUMMARY:' . $this->summary,
+      'URL:' . $this->url,
+      'DTSTAMP:' . $this->format_timestamp('now'),
+      'UID:' . uniqid(),
+      'END:VEVENT',
+      'END:VCALENDAR'
     );
-
-    // Build ICS properties - add header
-    $props = array();
-    foreach($this->properties as $k => $v) {
-      $props[strtoupper($k . ($k === 'url' ? ';VALUE=URI' : ''))] = $v;
-    }
-
-    // Set some default values
-    $props['DTSTAMP'] = $this->format_timestamp('now');
-    $props['UID'] = uniqid();
-
-    // Append properties
-    foreach ($props as $k => $v) {
-      $ics_props[] = "$k:$v";
-    }
-
-    // Build ICS properties - add footer
-    $ics_props[] = 'END:VEVENT';
-    $ics_props[] = 'END:VCALENDAR';
-
     return $ics_props;
-  }
-
-  private function sanitize_val($val, $key = false) {
-    switch($key) {
-      case 'dtend':
-      case 'dtstamp':
-      case 'dtstart':
-        $val = $this->format_timestamp($val);
-        break;
-      default:
-        $val = $this->escape_string($val);
-    }
-
-    return $val;
   }
 
   private function format_timestamp($timestamp) {
