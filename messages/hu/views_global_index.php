0ae7e55b8fcae8ea94f8261df6c37b9a1c2ceabd
HumHub Translations <info@humhub.com>
2018-03-25 21:04:22 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/views_global_index.php
+++ messages/hu/views_global_index.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Filter</strong> events' => '',
-  '<strong>Select</strong> calendars' => '',
-  'Already responded' => '',
-  'Followed spaces' => '',
-  'Followed users' => '',
-  'I\'m attending' => '',
-  'My events' => '',
+  '<strong>Filter</strong> events' => '<strong>Esemény</strong> szűrése',
+  '<strong>Select</strong> calendars' => '<strong>Válassz</strong> naptár(aka)t',
+  'Already responded' => 'Már válaszolt',
+  'Followed spaces' => 'Követett témakörök',
+  'Followed users' => 'Követett felhasználók',
+  'I\'m attending' => 'Részt veszek',
+  'My events' => 'Eseményeim',
   'My profile' => 'Profilom',
   'My spaces' => 'Témaköreim',
-  'Not responded yet' => '',
+  'Not responded yet' => 'Még nem válaszolt',
 );
