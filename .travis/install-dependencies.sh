7cd6e3485e401ae432958006b6a4379467b0d244
Daniel Kesselberg <mail@danielkesselberg.de>
2018-05-09 20:11:07 +0200

    Disable output for chromedriver

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -6,7 +6,7 @@ set -ev
 # Install chomedriver
 wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
 unzip -d $HOME chromedriver_linux64.zip
-$HOME/chromedriver --url-base=/wd/hub &
+$HOME/chromedriver --url-base=/wd/hub --silent &
 
 # Install composer package
 composer global require fxp/composer-asset-plugin
