ac2e1c53aaa03ebbb481b10bb00081873e9b6645
Daniel Kesselberg <mail@danielkesselberg.de>
2018-05-09 19:07:00 +0200

    Start php test server

--- .travis/setup-humhub.sh
+++ .travis/setup-humhub.sh
@@ -9,11 +9,7 @@ mkdir ${HUMHUB_PATH}
 cd ${HUMHUB_PATH}
 
 git clone --branch master --depth 1 https://github.com/humhub/humhub.git .
-composer install --prefer-dist --no-interaction --quiet
-
-php -S 127.0.0.1:8080 &> /dev/null &
-sleep 5
-curl --fail --head http://127.0.0.1:8080/index-test.php
+composer install --prefer-dist --no-interaction
 
 cd ${HUMHUB_PATH}/protected/humhub/tests
 
@@ -24,5 +20,9 @@ mysql -e 'CREATE DATABASE humhub_test;'
 php codeception/bin/yii migrate/up --includeModuleMigrations=1 --interactive=0
 php codeception/bin/yii installer/auto
 php codeception/bin/yii search/rebuild
+
+php --server 127.0.0.1:8080 --docroot ${HUMHUB_PATH} &> /dev/null &
+sleep 5
+curl --fail --head http://127.0.0.1:8080/index-test.php
 
 php ${HUMHUB_PATH}/protected/vendor/bin/codecept build
