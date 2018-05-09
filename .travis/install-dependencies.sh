c979ae5d021cbdcd31a7ebe85bd1bf7ef897d977
Daniel Kesselberg <mail@danielkesselberg.de>
2018-05-09 22:45:16 +0200

    Set +x on setup

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -5,7 +5,8 @@ set -ev
 
 # Install chomedriver
 curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip \
-    && unzip -o -d $HOME chromedriver_linux64.zip
+    && unzip -o -d $HOME chromedriver_linux64.zip \
+	&& chmod +x $HOME/chromedriver
 
 # Install composer package
 composer global require fxp/composer-asset-plugin
