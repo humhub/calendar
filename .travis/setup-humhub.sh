0712c45a4d962ae6c9e90423ae139321a8965168
Daniel Kesselberg <mail@danielkesselberg.de>
2018-05-09 21:05:46 +0200

    Test against Humhub 1.3

--- .travis/setup-humhub.sh
+++ .travis/setup-humhub.sh
@@ -8,7 +8,7 @@ old=$(pwd)
 mkdir ${HUMHUB_PATH}
 cd ${HUMHUB_PATH}
 
-git clone --branch master --depth 1 https://github.com/humhub/humhub.git .
+git clone --branch ${HUMHUB_VERSION} --depth 1 https://github.com/humhub/humhub.git .
 composer install --prefer-dist --no-interaction
 
 cd ${HUMHUB_PATH}/protected/humhub/tests
@@ -20,5 +20,9 @@ mysql -e 'CREATE DATABASE humhub_test;'
 php codeception/bin/yii migrate/up --includeModuleMigrations=1 --interactive=0
 php codeception/bin/yii installer/auto
 php codeception/bin/yii search/rebuild
+
+php --server 127.0.0.1:8080 --docroot ${HUMHUB_PATH} &>/dev/null &
+sleep 5
+curl --fail --head http://127.0.0.1:8080/index-test.php
 
 php ${HUMHUB_PATH}/protected/vendor/bin/codecept build
