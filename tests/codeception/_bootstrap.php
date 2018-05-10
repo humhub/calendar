338508d78be8aaf0aa5125a8b2cfcf24a68ce50b
Daniel Kesselberg <mail@danielkesselberg.de>
2018-05-10 16:33:03 +0200

    Add badge for Travis Clean bootstrap.php

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -1,29 +1,35 @@
 <?php
 /**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+/*
  * This is the initial test bootstrap, which will load the default test bootstrap from the humhub core
  */
 
 $testRoot = dirname(__DIR__);
+
 \Codeception\Configuration::append(['test_root' => $testRoot]);
+echo 'Module root: ' . $testRoot . PHP_EOL;
 
-$humhubPath = getenv("HUMHUB_PATH");
-
-// If no environment path was set, we assume residing in default the modules directory
-if($humhubPath == null) {
-    $testCfg = require_once($testRoot.'/config/test.php');
-    if(isset($testCfg['humhub_root'])) {
-        $humhubPath = $testCfg['humhub_root'];
+$humhubPath = getenv('HUMHUB_PATH');
+if ($humhubPath === false) {
+    // If no environment path was set, we assume residing in default the modules directory
+    $moduleConfig = require $testRoot . '/config/test.php';
+    if (isset($moduleConfig['humhub_root'])) {
+        $humhubPath = $moduleConfig['humhub_root'];
     } else {
-        $humhubPath = dirname(__DIR__).'../../../../';
+        $humhubPath = dirname(__DIR__, 5);
     }
 }
 
 \Codeception\Configuration::append(['humhub_root' => $humhubPath]);
+echo 'HumHub Root: ' . $humhubPath . PHP_EOL;
 
 // Load test configuration (/config/test.php or /config/env/<environment>/test.php
-$cfg = require($humhubPath . '/protected/humhub/tests/codeception/_loadConfig.php');
-
-print_r('Using HumHub Root: ' . $cfg['humhub_root']);
+$globalConfig = require $humhubPath . '/protected/humhub/tests/codeception/_loadConfig.php';
 
 // Load default test bootstrap (initialize Yii...)
-require_once($cfg['humhub_root'] . '/protected/humhub/tests/codeception/_bootstrap.php');
+require $globalConfig['humhub_root'] . '/protected/humhub/tests/codeception/_bootstrap.php';
