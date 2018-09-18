ba5216fd8ed56306878c65b72bc501ecc0667033
HumHub Translations <info@humhub.com>
2018-09-18 11:23:29 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/notifications_views_CanceledEvent.php
+++ messages/nl/notifications_views_CanceledEvent.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '{displayName} just added you to event "{contentTitle}".' => '',
-    '{displayName} just updated event {contentTitle}.' => '',
-    '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} annuleerde gebeurtenis "{contentTitle}" in ruimte {spaceName}.',
-    '{displayName} canceled event "{contentTitle}".' => '{displayName} annuleerde gebeurtenis "{contentTitle}".',
-    '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} werkte zojuist gebeurtenis "{contentTitle}" in ruimte {spaceName} bij.',
-    '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} heropende gebeurtenis "{contentTitle}" in ruimte {spaceName}.',
-    '{displayName} reopened event "{contentTitle}".' => '{displayName} heropende gebeurtenis "{contentTitle}".',
-];
+return array (
+  '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} annuleerde "{contentTitle}" in ruimte {spaceName}.',
+  '{displayName} canceled event "{contentTitle}".' => '{displayName} annuleerde "{contentTitle}".',
+  '{displayName} just added you to event "{contentTitle}".' => '{displayName} heeft u zojuist aan "{contentTitle}" toegevoegd.',
+  '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} werkte zojuist "{contentTitle}" in ruimte {spaceName} bij.',
+  '{displayName} just updated event {contentTitle}.' => '{displayName} heeft {contentTitle} zojuist bijgewerkt.',
+  '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} heropende "{contentTitle}" in ruimte {spaceName}.',
+  '{displayName} reopened event "{contentTitle}".' => '{displayName} heropende  "{contentTitle}".',
+);
