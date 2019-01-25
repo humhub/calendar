a43c8a5a0d10f6a542036a62f2f2eb3f444fc109
HumHub Translations <info@humhub.com>
2019-01-25 12:06:45 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/nb_no/notifications_views_CanceledEvent.php
+++ messages/nb_no/notifications_views_CanceledEvent.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '{displayName} just added you to event "{contentTitle}".' => '',
-    '{displayName} just updated event {contentTitle}.' => '',
-    '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} har avlyst aktivitet "{contentTitle}" i gruppen {spaceName}.',
-    '{displayName} canceled event "{contentTitle}".' => '{displayName} har avlyst aktivitet "{contentTitle}".',
-    '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} har oppdatert aktivitet "{contentTitle}" i gruppen {spaceName}.',
-    '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} har gjenåpnet aktivitet "{contentTitle}" i gruppen {spaceName}.',
-    '{displayName} reopened event "{contentTitle}".' => '{displayName} har gjenåpnet aktivitet "{contentTitle}".',
-];
+return array (
+  '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} har avlyst aktivitet "{contentTitle}" i gruppen {spaceName}.',
+  '{displayName} canceled event "{contentTitle}".' => '{displayName} har avlyst aktivitet "{contentTitle}".',
+  '{displayName} just added you to event "{contentTitle}".' => '{displayName} La deg til et arrangement "{contentTitle}".',
+  '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} har oppdatert aktivitet "{contentTitle}" i gruppen {spaceName}.',
+  '{displayName} just updated event {contentTitle}.' => '{displayName} oppdaterte arrangementet {contentTitle}.',
+  '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} har gjenåpnet aktivitet "{contentTitle}" i gruppen {spaceName}.',
+  '{displayName} reopened event "{contentTitle}".' => '{displayName} har gjenåpnet aktivitet "{contentTitle}".',
+);
