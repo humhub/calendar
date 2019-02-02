d1bfa4317bffadaf2eb21583f8e60f43da7836fc
HumHub Translations <info@humhub.com>
2019-02-02 18:02:08 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fi/notifications_views_CanceledEvent.php
+++ messages/fi/notifications_views_CanceledEvent.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '{displayName} just added you to event "{contentTitle}".' => '',
-    '{displayName} just updated event {contentTitle}.' => '',
-    '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} perui tapahtuman "{contentTitle}" sivulla {spaceName}.',
-    '{displayName} canceled event "{contentTitle}".' => '{displayName} peruutti tapahtuman "{contentTitle}".',
-    '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} päivitti tapahtumaa "{contentTitle}" sivulla {spaceName}.',
-    '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} avasi uudelleen tapahtuman "{contentTitle}" sivulla {spaceName}.',
-    '{displayName} reopened event "{contentTitle}".' => '{displayName} avasi uudestaan tapahtuman "{contentTitle}".',
-];
+return array (
+  '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} perui tapahtuman "{contentTitle}" sivulla {spaceName}.',
+  '{displayName} canceled event "{contentTitle}".' => '{displayName} peruutti tapahtuman "{contentTitle}".',
+  '{displayName} just added you to event "{contentTitle}".' => '{displayName} lisäsi sinut juuri tapahtumaan "{contentTitle}"',
+  '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} päivitti tapahtumaa "{contentTitle}" sivulla {spaceName}.',
+  '{displayName} just updated event {contentTitle}.' => '{displayName} juuri päivitti tapahtumaa {contentTitle}.',
+  '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} avasi uudelleen tapahtuman "{contentTitle}" sivulla {spaceName}.',
+  '{displayName} reopened event "{contentTitle}".' => '{displayName} avasi uudestaan tapahtuman "{contentTitle}".',
+);
