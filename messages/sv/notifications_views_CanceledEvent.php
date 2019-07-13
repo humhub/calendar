e8ad23d6df7043c0c5c30f9474ec76fad7fb4fcf
HumHub Translations <info@humhub.com>
2019-07-13 10:31:09 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/notifications_views_CanceledEvent.php
+++ messages/sv/notifications_views_CanceledEvent.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '{displayName} just added you to event "{contentTitle}".' => '',
-    '{displayName} just updated event {contentTitle}.' => '',
-    '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} raderade händelsen "{contentTitle}" i nätverket {spaceName}.',
-    '{displayName} canceled event "{contentTitle}".' => '{displayName} raderade händelsen "{contentTitle}"',
-    '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} uppdaterade händelsen "{contentTitle}" i nätverket {spaceName}.',
-    '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} återaktiverade händelsen "{contentTitle}" i nätverket {spaceName}.',
-    '{displayName} reopened event "{contentTitle}".' => '{displayName} återaktiverade händelsen "{contentTitle}" ',
-];
+return array (
+  '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '{displayName} raderade händelsen "{contentTitle}" i nätverket {spaceName}.',
+  '{displayName} canceled event "{contentTitle}".' => '{displayName} raderade händelsen "{contentTitle}"',
+  '{displayName} just added you to event "{contentTitle}".' => '{displayName} lade jut till dig till "{contentTitle}".',
+  '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '{displayName} uppdaterade händelsen "{contentTitle}" i nätverket {spaceName}.',
+  '{displayName} just updated event {contentTitle}.' => '{displayName} uppdaterade {contentTitle}.',
+  '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '{displayName} återaktiverade händelsen "{contentTitle}" i nätverket {spaceName}.',
+  '{displayName} reopened event "{contentTitle}".' => '{displayName} återaktiverade händelsen "{contentTitle}"',
+);
