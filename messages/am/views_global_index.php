f787dda71f38288cc98d10c10fd2615eb3b96bbe
HumHub Translations <info@humhub.com>
2019-09-23 15:03:46 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/am/views_global_index.php
+++ messages/am/views_global_index.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    '<strong>Filter</strong> events' => '',
-    '<strong>Select</strong> calendars' => '',
-    'Followed spaces' => '',
-    'Followed users' => '',
-    'I\'m attending' => '',
-    'My events' => '',
-    'My profile' => '',
-    'My spaces' => '',
-];
+return array (
+  '<strong>Filter</strong> events' => '',
+  '<strong>Select</strong> calendars' => '',
+  'Followed spaces' => '',
+  'Followed users' => '',
+  'I\'m attending' => '',
+  'My events' => '',
+  'My profile' => 'የግል መረጃዬ',
+  'My spaces' => '',
+);
