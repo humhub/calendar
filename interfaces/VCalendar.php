1b1bd6462c6ea26cb1720140811ccea1f0c32766
buddh4 <julian.harrer@humhub.com>
2020-01-16 11:50:07 +0100

    Fixed recurrent instance event ical export

--- interfaces/VCalendar.php
+++ interfaces/VCalendar.php
@@ -147,15 +147,21 @@ class VCalendar extends Model
         }
 
         if ($item instanceof RecurrentEventIF && RecurrenceHelper::isRecurrent($item)) {
-            $result['RRULE'] = $item->getRRule();
 
-            // Note: VObject supports the EXDATE property for exclusions, but not yet the RDATE and EXRULE properties
-            if (!empty($item->getExdate())) {
-                $result['EXDATE'] = [];
-                foreach (explode(',', $item->getExdate()) as $exdate) {
-                    $result['EXDATE'][] = $exdate;
+            if(RecurrenceHelper::isRecurrentRoot($item)) {
+                $result['RRULE'] = $item->getRRule();
+
+                // Note: VObject supports the EXDATE property for exclusions, but not yet the RDATE and EXRULE properties
+                if (!empty($item->getExdate())) {
+                    $result['EXDATE'] = [];
+                    foreach (explode(',', $item->getExdate()) as $exdate) {
+                        $result['EXDATE'][] = $exdate;
+                    }
                 }
+            } else if(RecurrenceHelper::isRecurrentInstance($item)) {
+                $result['RECURRENCE-ID'] = $item->getRecurrenceId();
             }
+
         } else {
             $this->setLegacyRecurrentData($item, $result);
 
