34c5ab781f8cc9501daa515762fdcff5598c49b7
HumHub Translations <info@humhub.com>
2020-01-28 15:05:39 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/el/notifications_views_CanceledEvent.php
+++ messages/el/notifications_views_CanceledEvent.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => '',
-    '{displayName} canceled event "{contentTitle}".' => '',
-    '{displayName} just added you to event "{contentTitle}".' => '',
-    '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => '',
-    '{displayName} just updated event {contentTitle}.' => '',
-    '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => '',
-    '{displayName} reopened event "{contentTitle}".' => '',
-];
+return array (
+  '{displayName} canceled event "{contentTitle}" in space {spaceName}.' => 'Ο/η {displayName} ακύρωσε εκδήλωση "{contentTitle}" στον χώρο {spaceName}.',
+  '{displayName} canceled event "{contentTitle}".' => 'Ο/η {displayName} ακύρωσε εκδήλωση "{contentTitle}".',
+  '{displayName} just added you to event "{contentTitle}".' => 'Ο/η {displayName} Μόλις σας πρόσθεσε στην εκδήλωση "{contentTitle}".',
+  '{displayName} just updated event "{contentTitle}" in space {spaceName}.' => 'Ο/η {displayName} μόλις ενημέρωσε εκδήλωση "{contentTitle}" στον χώρο {spaceName}.',
+  '{displayName} just updated event {contentTitle}.' => 'Ο/η {displayName} μόλις ενημέρωσε εκδήλωση {contentTitle}.',
+  '{displayName} reopened event "{contentTitle}" in space {spaceName}.' => 'Ο/η {displayName} επανάνοιξε εκδήλωση "{contentTitle}" στον χώρο {spaceName}.',
+  '{displayName} reopened event "{contentTitle}".' => 'Ο/η {displayName} επανάνοιξε εκδήλωση "{contentTitle}".',
+);
