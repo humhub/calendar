a5242a3174833dacc2fbeb68af813be3cfc17ef5
buddh4 <mail@jharrer.de>
2020-04-15 18:22:55 +0200

    Changed travis chromedriver version to 81.0.4044.69

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -4,11 +4,6 @@
 set -ev
 
 # Install chomedriver
-curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip \
+curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip \
     && unzip -o -d $HOME chromedriver_linux64.zip \
 	&& chmod +x $HOME/chromedriver
-
-# Install composer package
-if [ ${HUMHUB_VERSION} = "v1.2" ]; then
-    composer global require fxp/composer-asset-plugin
-fi
