110e7d3f3d1393ca442b2bdcca7f7c7eb537a6bb
HumHub Translations <info@humhub.com>
2021-02-03 10:23:48 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/views_entry_view.php
+++ messages/fr/views_entry_view.php
@@ -1,17 +1,7 @@
 <?php
-<<<<<<< HEAD
 return array (
   'Additional information' => 'Informations additionnelles :',
   'Attend' => 'Participer',
   'Decline' => 'Décliner',
   'Maybe' => 'Peut-être',
 );
-=======
-
-return [
-    'Attend' => 'Participer',
-    'Decline' => 'Décliner',
-    'Maybe' => 'Peut-être',
-    'Additional information' => '',
-];
->>>>>>> c3c436c79dd3f37b02ac15dac2525555aa5f6aa4
