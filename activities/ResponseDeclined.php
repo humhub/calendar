9a782d37d460a8213c90189e1596b19f937049d3
funkycram <contact@marc.fun>
2021-08-16 10:55:59 +0200

    Enh #187: Possibility to disable participation activities in the mail summary

--- activities/ResponseDeclined.php
+++ activities/ResponseDeclined.php
@@ -9,16 +9,33 @@
 namespace humhub\modules\calendar\activities;
 
 use humhub\modules\activity\components\BaseActivity;
+use humhub\modules\activity\interfaces\ConfigurableActivityInterface;
+use Yii;
 
 /**
  * ResponseDeclined Activity
  *
  * @author luke
  */
-class ResponseDeclined extends BaseActivity
+class ResponseDeclined extends BaseActivity implements ConfigurableActivityInterface
 {
 
     public $viewName = 'response_declined';
     public $moduleId = 'calendar';
 
+    /**
+     * @inheritdoc
+     */
+    public function getTitle()
+    {
+        return Yii::t('CalendarModule.activities', 'Calendar: decline');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDescription()
+    {
+        return Yii::t('CalendarModule.activities', 'Whenever someone declines to participate in an event.');
+    }
 }
---
+++ messages/fr/activities.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Calendar: attend' => 'Calendrier : participe',
+    'Calendar: decline' => 'Calendrier : ne participe pas',
+    'Calendar: maybe' => 'Calendrier : participe peut-être',
+    'Whenever someone declines to participate in an event.' => 'Lorsque quelqu\'un indique ne pas participer à un événement.',
+    'Whenever someone may be participating in an event.' => 'Lorsque quelqu\'un participera peut-être à un événement.',
+    'Whenever someone participates in an event.' => 'Lorsque quelqu\'un participe à un événement.',
+];
