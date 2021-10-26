acde4272cbabcbca26b16adbbd34161c151f0b3a
HumHub Translations <info@humhub.com>
2021-10-26 14:30:58 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/activities.php
+++ messages/de/activities.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Calendar: attend' => '',
-    'Calendar: decline' => '',
-    'Calendar: maybe' => '',
-    'Whenever someone declines to participate in an event.' => '',
-    'Whenever someone may be participating in an event.' => '',
-    'Whenever someone participates in an event.' => '',
-];
+return array (
+  'Calendar: attend' => 'Kalender: Annehmen',
+  'Calendar: decline' => 'Kalender: Ablehnen',
+  'Calendar: maybe' => 'Kalender: Vielleicht',
+  'Whenever someone declines to participate in an event.' => 'Wann immer jemand die Teilnahme an einem Termin ablehnt.',
+  'Whenever someone may be participating in an event.' => 'Wann immer jemand sich unser über die Teilnahme an einem Termin ist.',
+  'Whenever someone participates in an event.' => 'Wann immer jemand die Teilnahme an einem Termin bestätigt.',
+);
