74d0132a2402f155a5a5a79c6488eecfd9618f9c
HumHub Translations <hello@humhub.com>
2023-06-23 12:28:01 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sr/views_entry_view.php
+++ messages/sr/views_entry_view.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Additional information' => '',
-    'All' => '',
-    'Attend' => '',
-    'Decline' => '',
-    'Filter' => '',
-    'Maybe' => '',
-    'Participants' => '',
-    'You are invited, please select your role:' => '',
-];
+return array (
+  'Additional information' => '',
+  'All' => 'Све',
+  'Attend' => '',
+  'Decline' => '',
+  'Filter' => '',
+  'Maybe' => '',
+  'Participants' => '',
+  'You are invited, please select your role:' => '',
+);
