d2edd332eef82950fc19aaf6e3b2145d14fc51a5
HumHub Translations <hello@humhub.com>
2024-02-23 19:29:03 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/views_global_index.php
+++ messages/ja/views_global_index.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Followed spaces' => 'フォローされたスペース',
-    'Followed users' => 'フォローされたユーザー',
-    'I\'m attending' => '参加する',
-    'My events' => '自分のイベント',
-    'My profile' => '私のプロフィール',
-    'My spaces' => 'マイスペース',
-    'Filter events' => '',
-    'Select calendars' => '',
-];
+return array (
+  'Filter events' => 'フィルターイベント',
+  'Followed spaces' => 'フォローされたスペース',
+  'Followed users' => 'フォローされたユーザー',
+  'I\'m attending' => '参加する',
+  'My events' => '自分のイベント',
+  'My profile' => '私のプロフィール',
+  'My spaces' => 'マイスペース',
+  'Select calendars' => 'カレンダーを選択します',
+);
--- messages/nl/views_global_index.php
+++ messages/nl/views_global_index.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Followed spaces' => 'Gevolgde ruimtes',
-    'Followed users' => 'Gevolgde gebruikers',
-    'I\'m attending' => 'Ik ben aanwezig',
-    'My events' => 'Mijn gebeurtenissen',
-    'My profile' => 'Mijn profiel',
-    'My spaces' => 'Mijn ruimtes',
-    'Filter events' => '',
-    'Select calendars' => '',
-];
+return array (
+  'Filter events' => 'Gebeurtenissen filteren',
+  'Followed spaces' => 'Gevolgde ruimtes',
+  'Followed users' => 'Gevolgde gebruikers',
+  'I\'m attending' => 'Ik ben aanwezig',
+  'My events' => 'Mijn gebeurtenissen',
+  'My profile' => 'Mijn profiel',
+  'My spaces' => 'Mijn ruimtes',
+  'Select calendars' => 'Kies kalenders',
+);
