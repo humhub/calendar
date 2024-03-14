8669d9c737fe60732f6970f3796be5d8c7714290
HumHub Translations <hello@humhub.com>
2024-03-14 15:29:39 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/views_global_index.php
+++ messages/hu/views_global_index.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Followed spaces' => 'Követett közösségek',
-    'Followed users' => 'Követett felhasználók',
-    'I\'m attending' => 'Részt veszek',
-    'My events' => 'Eseményeim',
-    'My profile' => 'Profilom',
-    'My spaces' => 'Közösségeim',
-    'Filter events' => '',
-    'Select calendars' => '',
-];
+return array (
+  'Filter events' => 'Események szűrése',
+  'Followed spaces' => 'Követett közösségek',
+  'Followed users' => 'Követett felhasználók',
+  'I\'m attending' => 'Részt veszek',
+  'My events' => 'Eseményeim',
+  'My profile' => 'Profilom',
+  'My spaces' => 'Közösségeim',
+  'Select calendars' => 'Naptárak kiválasztása',
+);
