5e389c4a15c7321f1ebfda53021ca4749949897b
HumHub Translations <hello@humhub.com>
2024-09-09 10:28:54 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/mail.php
+++ messages/ja/mail.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'View Online: {url}' => 'オンラインで表示：{url}',
-    'Description' => '',
-    'Location' => '',
-    'Organizer' => '',
-    'Participants info' => '',
-    'Starting' => '',
-];
+return array (
+  'Description' => '説明',
+  'Location' => '場所',
+  'Organizer' => '主催者',
+  'Participants info' => '参加者情報',
+  'Starting' => '開始',
+  'View Online: {url}' => 'オンラインで表示：{url}',
+);
