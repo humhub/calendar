19d84c5579f02dea9104e991cb604f05566fc4bc
Yuriy Bakhtin <yurybakh@gmail.com>
2025-02-14 11:31:43 +0100

    Add author filter for custom page element

--- extensions/custom_pages/elements/CalendarEventsElement.php
+++ extensions/custom_pages/elements/CalendarEventsElement.php
@@ -9,19 +9,13 @@
 namespace humhub\modules\calendar\extensions\custom_pages\elements;
 
 use humhub\modules\calendar\models\CalendarEntry;
-use humhub\modules\custom_pages\modules\template\elements\BaseRecordsElement;
+use humhub\modules\custom_pages\modules\template\elements\BaseContentRecordsElement;
 use Yii;
-use yii\db\ActiveQuery;
 
 /**
  * Class to manage content records of the elements with Calendar events list
- *
- * Dynamic attributes:
- * @property array $space
- * @property array $topic
- * @property int $limit
  */
-class CalendarEventsElement extends BaseRecordsElement
+class CalendarEventsElement extends BaseContentRecordsElement
 {
     public const RECORD_CLASS = CalendarEntry::class;
     public string $subFormView = '@calendar/extensions/custom_pages/elements/views/calendars';
@@ -37,97 +31,10 @@ class CalendarEventsElement extends BaseRecordsElement
     /**
      * @inheritdoc
      */
-    protected function getDynamicAttributes(): array
-    {
-        return array_merge(parent::getDynamicAttributes(), [
-            'space' => null,
-            'topic' => null,
-            'limit' => null,
-        ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function attributeLabels()
     {
         return array_merge(parent::attributeLabels(), [
             'static' => Yii::t('CalendarModule.base', 'Select calendars'),
-            'space' => Yii::t('CalendarModule.base', 'Spaces'),
-            'topic' => Yii::t('CalendarModule.base', 'Topics'),
-            'limit' => Yii::t('CalendarModule.base', 'Limit'),
         ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function attributeHints()
-    {
-        return array_merge(parent::attributeHints(), [
-            'space' => Yii::t('CalendarModule.base', 'Leave empty to list calendar events from all spaces.'),
-        ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getTypes(): array
-    {
-        return array_merge(parent::getTypes(), [
-            'options' => Yii::t('CalendarModule.base', 'Calendar events with specific criteria'),
-        ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function isConfigured(): bool
-    {
-        return parent::isConfigured() || $this->type === 'options';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function getQuery(): ActiveQuery
-    {
-        $query = CalendarEntry::find()->readable();
-
-        return match ($this->type) {
-            'options' => $this->filterOptions($query),
-            default => $this->filterStatic($query),
-        };
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function filterStatic(ActiveQuery $query): ActiveQuery
-    {
-        return $query->andWhere(['calendar_entry.id' => $this->static]);
-    }
-
-    protected function filterOptions(ActiveQuery $query): ActiveQuery
-    {
-        empty($this->space)
-            ? $query->andWhere(['IS NOT', 'space.id', null])
-            : $query->andWhere(['space.guid' => $this->space]);
-
-        if (!empty($this->topic)) {
-            $query->leftJoin('content_tag_relation', 'content_tag_relation.content_id = content.id')
-                ->andWhere(['content_tag_relation.tag_id' => $this->topic]);
-        }
-
-        return $query;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function isCacheable(): bool
-    {
-        // Don't cache because the filter `CalendarEntry::find()->readable()` is used here
-        return true;
     }
 }
--- extensions/custom_pages/elements/views/calendars.php
+++ extensions/custom_pages/elements/views/calendars.php
@@ -11,6 +11,7 @@ use humhub\modules\calendar\widgets\CalendarEntryPicker;
 use humhub\modules\space\widgets\SpacePickerField;
 use humhub\modules\topic\widgets\TopicPicker;
 use humhub\modules\ui\form\widgets\ActiveForm;
+use humhub\modules\user\widgets\UserPickerField;
 
 /* @var CalendarEventsElement $model */
 /* @var ActiveForm $form */
@@ -21,6 +22,7 @@ use humhub\modules\ui\form\widgets\ActiveForm;
 
 <div class="records-content-form-fields" data-type="options">
     <?= $form->field($model, 'space')->widget(SpacePickerField::class) ?>
+    <?= $form->field($model, 'author')->widget(UserPickerField::class) ?>
     <?= $form->field($model, 'topic')->widget(TopicPicker::class) ?>
     <?= $form->field($model, 'limit') ?>
 </div>
