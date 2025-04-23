30bd8cecc3f29b8f3c85b1f77117df5ab0aa3305
gevorgmansuryan <gevorgmansuryan@gmail.com>
2025-04-24 01:22:54 +0400

    Calendar Export #577 - WIP - caldav - Added links to ui

--- views/config/export/caldav.php
+++ views/config/export/caldav.php
@@ -14,24 +14,24 @@ use yii\helpers\Url;
         <?= Html::label(Yii::t('CalendarModule.export', 'CalDAV URL')); ?>
         <?= Html::textInput(null, Url::to(['/calendar/remote/cal-dav'], true), ['disabled' => true, 'class' => 'form-control']) ?>
         <div class="text-right help-block">
-            <div id="url_1" class="hidden"><?= Url::to(['/calendar/remote/cal-dav'], true) ?></div>
-            <?= Link::withAction(Yii::t('CalendarModule.export', 'Copy to clipboard'), 'copyToClipboard', null, '#url_1')->icon('fa-clipboard')->style('color:#777') ?>
+            <div id="url_2" class="hidden"><?= Url::to(['/calendar/remote/cal-dav'], true) ?></div>
+            <?= Link::withAction(Yii::t('CalendarModule.export', 'Copy to clipboard'), 'copyToClipboard', null, '#url_2')->icon('fa-clipboard')->style('color:#777') ?>
         </div>
     </div>
     <div class="form-group">
         <?= Html::label(Yii::t('CalendarModule.export', 'CalDAV Base URL')); ?>
         <?= Html::textInput(null, Url::to(['/'], true), ['disabled' => true, 'class' => 'form-control']) ?>
         <div class="text-right help-block">
-            <div id="url_1" class="hidden"><?= Url::to(['/'], true) ?></div>
-            <?= Link::withAction(Yii::t('CalendarModule.export', 'Copy to clipboard'), 'copyToClipboard', null, '#url_1')->icon('fa-clipboard')->style('color:#777') ?>
+            <div id="url_3" class="hidden"><?= Url::to(['/'], true) ?></div>
+            <?= Link::withAction(Yii::t('CalendarModule.export', 'Copy to clipboard'), 'copyToClipboard', null, '#url_3')->icon('fa-clipboard')->style('color:#777') ?>
         </div>
     </div>
     <div class="form-group">
         <?= Html::label(Yii::t('CalendarModule.export', 'Username')); ?>
         <?= Html::textInput(null, Yii::$app->user->identity->username, ['disabled' => true, 'class' => 'form-control']) ?>
         <div class="text-right help-block">
-            <div id="url_1" class="hidden"><?= Yii::$app->user->identity->username ?></div>
-            <?= Link::withAction(Yii::t('CalendarModule.export', 'Copy to clipboard'), 'copyToClipboard', null, '#url_1')->icon('fa-clipboard')->style('color:#777') ?>
+            <div id="url_4" class="hidden"><?= Yii::$app->user->identity->username ?></div>
+            <?= Link::withAction(Yii::t('CalendarModule.export', 'Copy to clipboard'), 'copyToClipboard', null, '#url_4')->icon('fa-clipboard')->style('color:#777') ?>
         </div>
     </div>
     <div class="form-group">
