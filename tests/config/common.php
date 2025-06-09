30176f2e22b8a0533cb9e4b4c5e8e05da63e6c59
gevorgmansuryan <gevorgmansuryan@gmail.com>
2025-06-10 00:55:26 +0400

    Calendar Export #577 - fix tests

--- tests/config/common.php
+++ tests/config/common.php
@@ -3,9 +3,5 @@
 /**
  * This config is shared by all suites (unit/functional/acceptance) and can be overwritten by a suite config (e.g. functional.php)
  */
-return [
-    'params' => [
-        'moduleAutoloadPaths' => ['/app/modules/humhub'],
-    ]
-];
+return [];
 
--- tests/config/test.php
+++ tests/config/test.php
@@ -2,7 +2,6 @@
 
 return [
     'modules' => ['calendar'],
-    'humhub_root' => '/app/humhub',
     'fixtures' => [
         'default',
         'calendar_entry' => 'humhub\modules\calendar\tests\codeception\fixtures\CalendarEntryFixture',
