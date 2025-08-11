4587e1244fa139736fa9edcbce08a565adcb02f4
HumHub Translations <hello@humhub.com>
2025-08-11 19:54:49 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/export.php
+++ messages/fr/export.php
@@ -1,14 +1,13 @@
 <?php
-
 return [
-    '<strong>Calendar</strong> export' => '',
-    'CalDAV Base URL' => '',
-    'CalDAV URL' => '',
-    'Copy to clipboard' => '',
-    'Password' => '',
-    'This CalDAV link lets you sync events both ways with your calendar app. It supports two-way sync-changes made in your calendar app will be reflected in HumHub, and vice versa. Some calendar apps require the base URL, while others only need the full CalDAV URL. Use the credentials from your HumHub account when prompted.' => '',
-    'This link can be added to your calendar for syncing events. iCal updates your app with new events but doesn’t sync changes back (one-way sync).' => '',
-    'Username' => '',
-    'Your HumHub password' => '',
-    'iCal URL' => '',
+  '<strong>Calendar</strong> export' => 'Exporter le <strong>calendrier</strong>',
+  'CalDAV Base URL' => 'URL CalDAV de base',
+  'CalDAV URL' => 'URL CalDAV',
+  'Copy to clipboard' => 'Copier dans le presse-papiers',
+  'Password' => 'Mot de passe',
+  'This CalDAV link lets you sync events both ways with your calendar app. It supports two-way sync-changes made in your calendar app will be reflected in HumHub, and vice versa. Some calendar apps require the base URL, while others only need the full CalDAV URL. Use the credentials from your HumHub account when prompted.' => 'Ce lien CalDAV vous permet de synchroniser les événements avec votre appli de calendrier dans les deux sens. La synchronisation est bidirectionnelle : les modifications effectuées dans votre appli de calendrier se reflèteront dans HumHub et vice versa. Certaines applis de calendrier requièrent l’URL de base, tandis que d’autres ont uniquement besoin de l’URL CalDav complète. Saisissez vos identifiants HumHub lorsqu’ils vous sont demandés.',
+  'This link can be added to your calendar for syncing events. iCal updates your app with new events but doesn’t sync changes back (one-way sync).' => 'Ce lien peut être ajouté à votre calendrier pour synchroniser les événements. iCal met à jour votre appli de calendrier en y insérant les nouveaux événements, mais les modifications dans l’autre sens ne sont pas synchronisées (synchronisation unidirectionnelle).',
+  'Username' => 'Nom d’utilisateur ou d’utilisatrice',
+  'Your HumHub password' => 'Votre mot de passe HumHub',
+  'iCal URL' => 'URL iCal',
 ];
