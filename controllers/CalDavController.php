75ee7883c20bbe47fdfa14d2024734be422d5475
GitHub Action <action@github.com>
2025-08-23 15:24:06 +0000

    Autocommit PHP CS Fixer

--- controllers/CalDavController.php
+++ controllers/CalDavController.php
@@ -63,9 +63,9 @@ class CalDavController extends Controller
 
         // Take control of error action only when called from Calendar Clients
         if (
-            !empty($accept) &&
-            $accept[0] !== 'text/html' &&
-            !empty(array_intersect($accept, ['*/*', 'text/xml', 'application/xml' . 'text/calendar', 'application/ics', 'text/plain']))
+            !empty($accept)
+            && $accept[0] !== 'text/html'
+            && !empty(array_intersect($accept, ['*/*', 'text/xml', 'application/xml' . 'text/calendar', 'application/ics', 'text/plain']))
         ) {
             if ($exception instanceof ForbiddenHttpException || $exception instanceof UnauthorizedHttpException) {
                 $this->response->statusCode = 401;
