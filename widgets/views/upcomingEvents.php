6ac689ee8d8f8b465bf14c684ba4d7e0eb5e93f8
Marc Farré <contact@marc.fun>
2025-12-10 16:33:50 +0000

    Remove extra space

--- widgets/views/upcomingEvents.php
+++ widgets/views/upcomingEvents.php
@@ -4,13 +4,14 @@ use humhub\libs\Helpers;
 use humhub\modules\calendar\interfaces\event\CalendarEventIF;
 use humhub\modules\calendar\models\CalendarDateFormatter;
 use humhub\modules\ui\icon\widgets\Icon;
+use humhub\widgets\bootstrap\Link;
 use humhub\widgets\PanelMenu;
 use yii\helpers\Html;
 
 /* @var $calendarEntries CalendarEventIF[] */
 /* @var $calendarUrl string */
 
-$link = Html::a(Icon::get('arrow-circle-right') . ' ' . Yii::t('CalendarModule.views', 'Open Calendar'), $calendarUrl);
+$link = Link::to(Yii::t('CalendarModule.views', 'Open Calendar'), $calendarUrl)->icon('arrow-circle-right');
 $extraMenus = Html::tag('li', $link);
 ?>
 <div class="panel calendar-upcoming-snippet" id="calendar-upcoming-events-snippet">
