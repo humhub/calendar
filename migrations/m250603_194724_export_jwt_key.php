d0538cdae553a5fc765da31d3c80a20e9212cc31
Lucas Bartholemy <lucas@bartholemy.com>
2026-06-16 17:14:15 +0200

    Fix: Make m250603_194724_export_jwt_key migration resilient when module is not bootstrapped

--- migrations/m250603_194724_export_jwt_key.php
+++ migrations/m250603_194724_export_jwt_key.php
@@ -1,13 +1,27 @@
 <?php
 
-use humhub\modules\calendar\models\ExportSettings;
 use yii\db\Migration;
 
 class m250603_194724_export_jwt_key extends Migration
 {
     public function safeUp()
     {
-        ExportSettings::instance()->save();
+        $jwtKey = $this->db->createCommand(
+            'SELECT value FROM setting WHERE module_id = :m AND name = :n',
+            [':m' => 'calendar', ':n' => 'jwtKey'],
+        )->queryScalar();
+
+        if (empty($jwtKey)) {
+            $jwtKey = Yii::$app->security->generateRandomString();
+
+            $this->upsert('setting', [
+                'module_id' => 'calendar',
+                'name' => 'jwtKey',
+                'value' => $jwtKey,
+            ], [
+                'value' => $jwtKey,
+            ]);
+        }
     }
 
     public function safeDown()
