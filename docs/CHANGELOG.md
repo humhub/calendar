b8c3578e72124ee2c5c87f095403ad1e8b016d3c
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2026-08-20 09:52:31 +0300

    Don't use a model saving in migration to avoid unknown column error

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+1.8.17 (Unreleased)
+----------------------
+- Fix #710: Don't use a model saving in migration to avoid unknown column error
+
 1.8.16 (July 17, 2026)
 ----------------------
 - Enh #708: Replace calendar filter checkboxes with dropdowns, Add new filter "Entire network"
--- migrations/m220217_141238_move_files.php
+++ migrations/m220217_141238_move_files.php
@@ -7,11 +7,19 @@
  */
 
 use humhub\modules\calendar\models\CalendarEntry;
-use humhub\modules\file\models\File;
 use yii\db\Migration;
+use yii\db\Query;
 
 /**
  * Class m220217_141238_move_files
+ *
+ * Note: This migration intentionally avoids the CalendarEntry ActiveRecord
+ * class's find()/save() calls (only its class name is referenced, via
+ * CalendarEntry::class, purely to build the object_model string). Historical
+ * data migrations must not depend on the *current* model's validation rules
+ * or schema expectations, since these may have moved on (e.g. columns added
+ * by later migrations that haven't run yet in this batch). Working with the
+ * table names and Query Builder directly keeps this migration self-contained.
  */
 class m220217_141238_move_files extends Migration
 {
@@ -20,40 +28,65 @@ class m220217_141238_move_files extends Migration
      */
     public function safeUp()
     {
-        // Find all Calendar Entries with attached files
-        $entries = CalendarEntry::find()
+        // Find all Calendar Entries with attached stream files,
+        // joined with the file rows themselves.
+        // One row per (entry, file) pair.
+        $query = (new Query())
+            ->select([
+                'entryId' => 'calendar_entry.id',
+                'description' => 'calendar_entry.description',
+                'guid' => 'file.guid',
+                'file_name' => 'file.file_name',
+                'mime_type' => 'file.mime_type',
+            ])
+            ->from('calendar_entry')
             ->innerJoin('file', 'file.object_id = calendar_entry.id')
             ->where(['file.object_model' => CalendarEntry::class])
             ->andWhere(['file.show_in_stream' => 1]);
 
-        foreach ($entries->all() as $entry) {
-            /* @var CalendarEntry $entry */
+        // Group rows by Calendar Entry
+        $entries = [];
+        foreach ($query->each(100, $this->db) as $row) {
+            $entryId = $row['entryId'];
+            if (!isset($entries[$entryId])) {
+                $entries[$entryId] = [
+                    'description' => $row['description'],
+                    'files' => [],
+                ];
+            }
+            $entries[$entryId]['files'][] = $row;
+        }
+
+        foreach ($entries as $entryId => $entry) {
             $attachedFilesContent = '';
 
             // Convert attached files into content/inline files
-            foreach ($entry->fileManager->findStreamFiles() as $file) {
-                /* @var File $file */
+            foreach ($entry['files'] as $file) {
                 $attachedFilesContent .= "\r\n";
-                if (str_starts_with((string) $file->mime_type, 'image/')) {
+                if (str_starts_with((string) $file['mime_type'], 'image/')) {
                     // Image
-                    $attachedFilesContent .= '![](file-guid:' . $file->guid . ' "' . $file->file_name . '")';
+                    $attachedFilesContent .= '![](file-guid:' . $file['guid'] . ' "' . $file['file_name'] . '")';
                 } else {
-                    $attachedFilesContent .= '[' . $file->file_name . '](file-guid:' . $file->guid . ')';
+                    $attachedFilesContent .= '[' . $file['file_name'] . '](file-guid:' . $file['guid'] . ')';
                 }
             }
 
             // Append attached files in the end of entry description
-            $entry->description .= "\r\n" . $attachedFilesContent;
-            $entry->save();
+            $this->update(
+                'calendar_entry',
+                ['description' => $entry['description'] . "\r\n" . $attachedFilesContent],
+                ['id' => $entryId],
+            );
         }
 
         // Update all attached files to content mode
-        $this->execute(
-            'UPDATE file
-              SET show_in_stream = 0
-            WHERE object_model = :CalendarEntryClass
-              AND show_in_stream = 1',
-            ['CalendarEntryClass' => CalendarEntry::class],
+        $this->update(
+            'file',
+            ['show_in_stream' => 0],
+            [
+                'object_model' => CalendarEntry::class,
+                'show_in_stream' => 1,
+            ],
         );
     }
 
